93bad01d4c5e8d054090985e78a04826eebb1339
Erin Quinn <equinn125@gmail.com>
2021-07-21 10:39:54 -0600

    complete iteration 1

---
+++ lib/character.rb
@@ -0,0 +1,13 @@
+require'pry'
+class Character
+attr_reader :name, :actor, :salary, :character
+  def initialize(character)
+  
+    @name =  character.fetch(:name)
+    @actor = character.fetch(:actor)
+    @salary = character.fetch(:salary)
+    # binding.pry
+  end
+  # def create_character
+  # end
+end
---
+++ spec/character_spec.rb
@@ -0,0 +1,25 @@
+require './lib/character'
+require'pry'
+
+RSpec.describe Character do
+    it "exists" do
+      kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+      expect(kitt).to be_a(Character)
+      end
+      it "has a name" do
+        kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+        expect(kitt.name).to eq("KITT")
+      end
+      it "has a actor" do
+        kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+        expect(kitt.actor).to eq("William Daniels")
+      end
+      it "has a salary" do
+        kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+        expect(kitt.salary).to eq(1_000_000)
+      end
+  end
